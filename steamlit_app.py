bcbfdfcc2546364dfc9d31ddcd027117d86ccbff
mlainaud <manon.lainaud@kpconsulting.fr>
2025-03-20 13:44:58 +0000

    add search_on

--- steamlit_app.py
+++ steamlit_app.py
@@ -42,7 +42,7 @@ if ingredients_list:
         st.write('The search value for ', fruit,' is ', search_on, '.')
 
         st.subheader(fruit + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
         #st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
